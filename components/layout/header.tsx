589f86742dca852eb732461b5312da39f81a00b4
Chris Kinsman <chrisknsmn@gmail.com>
2024-09-16 10:26:35 -0400

    header width

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Header() {
   return (
     <header className="bg-background border-b">
-      <div className="container mx-auto px-4 py-4 flex justify-between items-center">
+      <div className="container mx-auto px-8 py-4 flex justify-between items-center">
         <Link href="/" className="text-2xl font-bold">
           Next Template
         </Link>
