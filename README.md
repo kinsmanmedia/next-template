dff0090cbbec2b9e33a04d1e3766d9a08b58b867
Chris Kinsman <chrisknsmn@gmail.com>
2024-09-16 18:34:28 -0400

    clerk role auth

--- README.md
+++ README.md
@@ -5,7 +5,3 @@ npm run dev
 ```
 
 Open [http://localhost:3000](http://localhost:3000).
-
-## todo
-- move sign-in to clerk folder
-- move sign-up to clerk folder
--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,45 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { NextResponse } from 'next/server'
 
-export default clerkMiddleware();
+const isDashboardRoute = createRouteMatcher(['/dashboard(.*)'])
+const isAdminRoute = createRouteMatcher(['/dashboard/admin(.*)'])
+
+type SessionMetadata = {
+  role?: string;
+};
+
+export default clerkMiddleware((auth, req) => {
+  if (isDashboardRoute(req)) {
+    // Check if the user is authenticated
+    if (!auth().userId) {
+      // User is not authenticated, redirect to homepage
+      const homepageUrl = new URL('/', req.url)
+      return NextResponse.redirect(homepageUrl)
+    }
+  }
+  if (isAdminRoute(req)) {
+    // Check if the user is authenticated and an admin
+    const { userId } = auth()
+    if (!userId) {
+      // User is not authenticated, redirect to homepage
+      const homepageUrl = new URL('/', req.url)
+      return NextResponse.redirect(homepageUrl)
+    }
+    // Check if the user is an admin
+    const isAdmin = (auth().sessionClaims?.metadata as SessionMetadata)?.role === 'admin'
+    if (!isAdmin) {
+      // User is not an admin, redirect to homepage or unauthorized page
+      const unauthorizedUrl = new URL('/', req.url)
+      return NextResponse.redirect(unauthorizedUrl)
+    }
+  }
+  // For all other routes, continue as normal
+  return NextResponse.next()
+})
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
-};
+}
